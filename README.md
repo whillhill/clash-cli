623b376e2d9fd5fd6c5da43a9b198ba166d07326
whillhill <ooooofish@126.com>
2025-07-27 23:49:03 +0800

    Initial commit: clash-cli - Linux 一键安装 Clash 工具

---
+++ README.md
@@ -0,0 +1,175 @@
+# clash-cli - Linux 一键安装 Clash
+
+![GitHub License](https://img.shields.io/github/license/whillhill/clash-cli)
+![GitHub top language](https://img.shields.io/github/languages/top/whillhill/clash-cli)
+![GitHub Repo stars](https://img.shields.io/github/stars/whillhill/clash-cli)
+
+![preview](resources/preview.png)
+
+- 默认安装 `mihomo` 内核，[可选安装](https://github.com/whillhill/clash-cli/wiki/FAQ#%E5%AE%89%E8%A3%85-clash-%E5%86%85%E6%A0%B8) `clash`。
+- 自动使用 [subconverter](https://github.com/tindy2013/subconverter) 进行本地订阅转换。
+- 多架构支持，适配主流 `Linux` 发行版：`CentOS 7.6`、`Debian 12`、`Ubuntu 24.04.1 LTS`。
+
+## 快速开始
+
+### 环境要求
+
+- 用户权限：`root`、`sudo`。
+- `shell` 支持：`bash`、`zsh`、`fish`。
+
+### 一键安装
+
+下述命令适用于 `x86_64` 架构，其他架构请戳：[一键安装-多架构](https://github.com/whillhill/clash-cli/wiki#%E4%B8%80%E9%94%AE%E5%AE%89%E8%A3%85-%E5%A4%9A%E6%9E%B6%E6%9E%84)
+
+```bash
+git clone --branch master --depth 1 https://gh-proxy.com/https://github.com/whillhill/clash-cli.git \
+  && cd clash-cli \
+  && sudo bash install.sh
+```
+
+> 如遇问题，请在查阅[常见问题](https://github.com/whillhill/clash-cli/wiki/FAQ)及 [issue](https://github.com/whillhill/clash-cli/issues?q=is%3Aissue) 未果后进行反馈。
+
+- 上述克隆命令使用了[加速前缀](https://gh-proxy.com/)，如失效请更换其他[可用链接](https://ghproxy.link/)。
+- 默认通过远程订阅获取配置进行安装。
+- 没有订阅？[click me](https://次元.net/auth/register?code=oUbI)
+
+### 命令一览
+
+执行 `clash-cli` 列出开箱即用的快捷命令。
+
+```bash
+$ clash-cli
+Usage:
+    clash-cli COMMAND [OPTION]
+
+Commands:
+    on                   开启代理
+    off                  关闭代理
+    ui                   面板地址
+    status               内核状况
+    proxy    [on|off]    系统代理
+    tun      [on|off]    Tun 模式
+    mixin    [-e|-r]     Mixin 配置
+    secret   [SECRET]    Web 密钥
+    update   [auto|log]  更新订阅
+```
+
+### 优雅启停
+
+```bash
+$ clash-cli on
+😼 已开启代理环境
+
+$ clash-cli off
+😼 已关闭代理环境
+```
+- 启停代理内核的同时，设置系统代理。
+- 亦可通过 `clash-cli proxy` 单独控制系统代理。
+
+### Web 控制台
+
+```bash
+$ clash-cli ui
+╔═══════════════════════════════════════════════╗
+║                😼 Web 控制台                  ║
+║═══════════════════════════════════════════════║
+║                                               ║
+║     🔓 注意放行端口：9090                      ║
+║     🏠 内网：http://192.168.0.1:9090/ui       ║
+║     🌏 公网：http://255.255.255.255:9090/ui   ║
+║     ☁️ 公共：http://board.zash.run.place      ║
+║                                               ║
+╚═══════════════════════════════════════════════╝
+
+$ clash-cli secret 666
+😼 密钥更新成功，已重启生效
+
+$ clash-cli secret
+😼 当前密钥：666
+```
+
+- 通过浏览器打开 Web 控制台，实现可视化操作：切换节点、查看日志等。
+- 控制台密钥默认为空，若暴露到公网使用建议更新密钥。
+
+### 更新订阅
+
+```bash
+$ clash-cli update https://example.com
+👌 正在下载：原配置已备份...
+🍃 下载成功：内核验证配置...
+🍃 订阅更新成功
+
+$ clash-cli update auto [url]
+😼 已设置定时更新订阅
+
+$ clash-cli update log
+✅ [2025-02-23 22:45:23] 订阅更新成功：https://example.com
+```
+
+- `clash-cli update` 会记住上次更新成功的订阅链接，后续执行无需再指定。
+- 可通过 `crontab -e` 修改定时更新频率及订阅链接。
+
+### `Tun` 模式
+
+```bash
+$ clash-cli tun
+😾 Tun 状态：关闭
+
+$ clash-cli tun on
+😼 Tun 模式已开启
+```
+
+- 作用：实现本机及 `Docker` 等容器的所有流量路由到 `clash` 代理、DNS 劫持等。
+- 原理：[clash-verge-rev](https://www.clashverge.dev/guide/term.html#tun)、 [clash.wiki](https://clash.wiki/premium/tun-device.html)。
+
+### `Mixin` 配置
+
+```bash
+$ clash-cli mixin
+😼 less 查看 mixin 配置
+
+$ clash-cli mixin -e
+😼 vim 编辑 mixin 配置
+
+$ clash-cli mixin -r
+😼 less 查看 运行时 配置
+```
+
+- 持久化：将自定义配置写在 `Mixin` 而不是原配置中，可避免更新订阅后丢失自定义配置。
+- 运行时配置是订阅配置和 `Mixin` 配置的并集。
+- 相同配置项优先级：`Mixin` 配置 > 订阅配置。
+
+### 卸载
+
+```bash
+sudo bash uninstall.sh
+```
+
+## 常见问题
+
+[wiki](https://github.com/whillhill/clash-cli/wiki/FAQ)
+
+## 引用
+
+- [Clash 知识库](https://clash.wiki/)
+- [Clash 家族下载](https://www.clash.la/releases/)
+- [Clash Premium 2023.08.17](https://downloads.clash.wiki/ClashPremium/)
+- [mihomo v1.19.2](https://github.com/MetaCubeX/mihomo)
+- [subconverter v0.9.0：本地订阅转换](https://github.com/tindy2013/subconverter)
+- [yacd v0.3.8：Web 控制台](https://github.com/haishanh/yacd)
+- [yq v4.45.1：处理 yaml](https://github.com/mikefarah/yq)
+
+## Star History
+
+<a href="https://www.star-history.com/#whillhill/clash-cli&Date">
+ <picture>
+   <source media="(prefers-color-scheme: dark)" srcset="https://api.star-history.com/svg?repos=whillhill/clash-cli&type=Date&theme=dark" />
+   <source media="(prefers-color-scheme: light)" srcset="https://api.star-history.com/svg?repos=whillhill/clash-cli&type=Date" />
+   <img alt="Star History Chart" src="https://api.star-history.com/svg?repos=whillhill/clash-cli&type=Date" />
+ </picture>
+</a>
+
+## 特别声明
+
+1. 编写本项目主要目的为学习和研究 `Shell` 编程，不得将本项目中任何内容用于违反国家/地区/组织等的法律法规或相关规定的其他用途。
+2. 本项目保留随时对免责声明进行补充或更改的权利，直接或间接使用本项目内容的个人或组织，视为接受本项目的特别声明。
---
+++ script/clash-cli.sh
@@ -0,0 +1,322 @@
+# shellcheck disable=SC2148
+# shellcheck disable=SC2155
+# clash-cli.sh - Main control script for clash-cli
+
+_set_system_proxy() {
+    local auth=$(sudo "$BIN_YQ" '.authentication[0] // ""' "$CLASH_CONFIG_RUNTIME")
+    [ -n "$auth" ] && auth=$auth@
+
+    local http_proxy_addr="http://${auth}127.0.0.1:${MIXED_PORT}"
+    local socks_proxy_addr="socks5h://${auth}127.0.0.1:${MIXED_PORT}"
+    local no_proxy_addr="localhost,127.0.0.1,::1"
+
+    export http_proxy=$http_proxy_addr
+    export https_proxy=$http_proxy
+    export HTTP_PROXY=$http_proxy
+    export HTTPS_PROXY=$http_proxy
+
+    export all_proxy=$socks_proxy_addr
+    export ALL_PROXY=$all_proxy
+
+    export no_proxy=$no_proxy_addr
+    export NO_PROXY=$no_proxy
+
+    sudo "$BIN_YQ" -i '.system-proxy.enable = true' "$CLASH_CONFIG_MIXIN"
+}
+
+_unset_system_proxy() {
+    unset http_proxy
+    unset https_proxy
+    unset HTTP_PROXY
+    unset HTTPS_PROXY
+    unset all_proxy
+    unset ALL_PROXY
+    unset no_proxy
+    unset NO_PROXY
+
+    sudo "$BIN_YQ" -i '.system-proxy.enable = false' "$CLASH_CONFIG_MIXIN"
+}
+
+_clash_on() {
+    _get_proxy_port
+    systemctl is-active "$BIN_KERNEL_NAME" >&/dev/null || {
+        sudo systemctl start "$BIN_KERNEL_NAME" >/dev/null || {
+            _failcat '启动失败: 执行 clash-cli status 查看日志'
+            return 1
+        }
+    }
+    _set_system_proxy
+    _okcat '已开启代理环境'
+}
+
+watch_proxy() {
+    # 新开交互式shell，且无代理变量时
+    [ -z "$http_proxy" ] && [[ $- == *i* ]] && {
+        # root用户自动开启代理环境（普通用户会触发sudo验证密码导致卡住）
+        _is_root && _clash_on
+    }
+}
+
+_clash_off() {
+    sudo systemctl stop "$BIN_KERNEL_NAME" && _okcat '已关闭代理环境' ||
+        _failcat '关闭失败: 执行 "clash-cli status" 查看日志' || return 1
+    _unset_system_proxy
+}
+
+_clash_restart() {
+    { _clash_off && _clash_on; } >&/dev/null
+}
+
+_clash_proxy() {
+    case "$1" in
+    on)
+        systemctl is-active "$BIN_KERNEL_NAME" >&/dev/null || {
+            _failcat '代理程序未运行，请执行 clash-cli on 开启代理环境'
+            return 1
+        }
+        _set_system_proxy
+        _okcat '已开启系统代理'
+        ;;
+    off)
+        _unset_system_proxy
+        _okcat '已关闭系统代理'
+        ;;
+    status)
+        local system_proxy_status=$(sudo "$BIN_YQ" '.system-proxy.enable' "$CLASH_CONFIG_MIXIN" 2>/dev/null)
+        [ "$system_proxy_status" = "false" ] && {
+            _failcat "系统代理：关闭"
+            return 1
+        }
+        _okcat "系统代理：开启
+http_proxy： $http_proxy
+socks_proxy：$all_proxy"
+        ;;
+    *)
+        cat <<EOF
+用法: clash-cli proxy [on|off|status]
+    on      开启系统代理
+    off     关闭系统代理
+    status  查看系统代理状态
+EOF
+        ;;
+    esac
+}
+
+_clash_status() {
+    sudo systemctl status "$BIN_KERNEL_NAME" "$@"
+}
+
+_clash_ui() {
+    _get_ui_port
+    # 公网ip
+    # ifconfig.me
+    local query_url='api64.ipify.org'
+    local public_ip=$(curl -s --noproxy "*" --connect-timeout 2 $query_url)
+    local public_address="http://${public_ip:-公网}:${UI_PORT}/ui"
+    # 内网ip
+    # ip route get 1.1.1.1 | grep -oP 'src \K\S+'
+    local local_ip=$(hostname -I | awk '{print $1}')
+    local local_address="http://${local_ip}:${UI_PORT}/ui"
+    printf "\n"
+    printf "╔═══════════════════════════════════════════════╗\n"
+    printf "║                %s                  ║\n" "$(_okcat 'Web 控制台')"
+    printf "║═══════════════════════════════════════════════║\n"
+    printf "║                                               ║\n"
+    printf "║     🔓 注意放行端口：%-5s                    ║\n" "$UI_PORT"
+    printf "║     🏠 内网：%-31s  ║\n" "$local_address"
+    printf "║     🌏 公网：%-31s  ║\n" "$public_address"
+    printf "║     ☁️  公共：%-31s  ║\n" "$URL_CLASH_UI"
+    printf "║                                               ║\n"
+    printf "╚═══════════════════════════════════════════════╝\n"
+    printf "\n"
+}
+
+_merge_config_restart() {
+    local backup="/tmp/rt.backup"
+    sudo cat "$CLASH_CONFIG_RUNTIME" 2>/dev/null | sudo tee $backup >&/dev/null
+    sudo "$BIN_YQ" eval-all '. as $item ireduce ({}; . *+ $item) | (.. | select(tag == "!!seq")) |= unique' \
+        "$CLASH_CONFIG_MIXIN" "$CLASH_CONFIG_RAW" "$CLASH_CONFIG_MIXIN" | sudo tee "$CLASH_CONFIG_RUNTIME" >&/dev/null
+    _valid_config "$CLASH_CONFIG_RUNTIME" || {
+        sudo cat $backup | sudo tee "$CLASH_CONFIG_RUNTIME" >&/dev/null
+        _error_quit "验证失败：请检查 Mixin 配置"
+    }
+    _clash_restart
+}
+
+_clash_secret() {
+    case "$#" in
+    0)
+        _okcat "当前密钥：$(sudo "$BIN_YQ" '.secret // ""' "$CLASH_CONFIG_RUNTIME")"
+        ;;
+    1)
+        sudo "$BIN_YQ" -i ".secret = \"$1\"" "$CLASH_CONFIG_MIXIN" || {
+            _failcat "密钥更新失败，请重新输入"
+            return 1
+        }
+        _merge_config_restart
+        _okcat "密钥更新成功，已重启生效"
+        ;;
+    *)
+        _failcat "密钥不要包含空格或使用引号包围"
+        ;;
+    esac
+}
+
+_tunstatus() {
+    local tun_status=$(sudo "$BIN_YQ" '.tun.enable' "${CLASH_CONFIG_RUNTIME}")
+    # shellcheck disable=SC2015
+    [ "$tun_status" = 'true' ] && _okcat 'Tun 状态：启用' || _failcat 'Tun 状态：关闭'
+}
+
+_tunoff() {
+    _tunstatus >/dev/null || return 0
+    sudo "$BIN_YQ" -i '.tun.enable = false' "$CLASH_CONFIG_MIXIN"
+    _merge_config_restart && _okcat "Tun 模式已关闭"
+}
+
+_tunon() {
+    _tunstatus 2>/dev/null && return 0
+    sudo "$BIN_YQ" -i '.tun.enable = true' "$CLASH_CONFIG_MIXIN"
+    _merge_config_restart
+    sleep 0.5s
+    sudo journalctl -u "$BIN_KERNEL_NAME" --since "1 min ago" | grep -E -m1 'unsupported kernel version|Start TUN listening error' && {
+        _tunoff >&/dev/null
+        _error_quit '不支持的内核版本'
+    }
+    _okcat "Tun 模式已开启"
+}
+
+_clash_tun() {
+    case "$1" in
+    on)
+        _tunon
+        ;;
+    off)
+        _tunoff
+        ;;
+    *)
+        _tunstatus
+        ;;
+    esac
+}
+
+_clash_update() {
+    local url=$(cat "$CLASH_CONFIG_URL")
+    local is_auto
+
+    case "$1" in
+    auto)
+        is_auto=true
+        [ -n "$2" ] && url=$2
+        ;;
+    log)
+        sudo tail "${CLASH_UPDATE_LOG}" 2>/dev/null || _failcat "暂无更新日志"
+        return 0
+        ;;
+    *)
+        [ -n "$1" ] && url=$1
+        ;;
+    esac
+
+    # 如果没有提供有效的订阅链接（url为空或者不是http开头），则使用默认配置文件
+    [ "${url:0:4}" != "http" ] && {
+        _failcat "没有提供有效的订阅链接：使用 ${CLASH_CONFIG_RAW} 进行更新..."
+        url="file://$CLASH_CONFIG_RAW"
+    }
+
+    # 如果是自动更新模式，则设置定时任务
+    [ "$is_auto" = true ] && {
+        sudo grep -qs 'clash-cli update' "$CLASH_CRON_TAB" || echo "0 0 */2 * * $_SHELL -i -c 'clash-cli update $url'" | sudo tee -a "$CLASH_CRON_TAB" >&/dev/null
+        _okcat "已设置定时更新订阅" && return 0
+    }
+
+    _okcat '👌' "正在下载：原配置已备份..."
+    sudo cat "$CLASH_CONFIG_RAW" | sudo tee "$CLASH_CONFIG_RAW_BAK" >&/dev/null
+
+    _rollback() {
+        _failcat '🍂' "$1"
+        sudo cat "$CLASH_CONFIG_RAW_BAK" | sudo tee "$CLASH_CONFIG_RAW" >&/dev/null
+        _failcat '❌' "[$(date +"%Y-%m-%d %H:%M:%S")] 订阅更新失败：$url" 2>&1 | sudo tee -a "${CLASH_UPDATE_LOG}" >&/dev/null
+        _error_quit
+    }
+
+    _download_config "$CLASH_CONFIG_RAW" "$url" || _rollback "下载失败：已回滚配置"
+    _valid_config "$CLASH_CONFIG_RAW" || _rollback "转换失败：已回滚配置，转换日志：$BIN_SUBCONVERTER_LOG"
+
+    _merge_config_restart && _okcat '🍃' '订阅更新成功'
+    echo "$url" | sudo tee "$CLASH_CONFIG_URL" >&/dev/null
+    _okcat '✅' "[$(date +"%Y-%m-%d %H:%M:%S")] 订阅更新成功：$url" | sudo tee -a "${CLASH_UPDATE_LOG}" >&/dev/null
+}
+
+_clash_mixin() {
+    case "$1" in
+    -e)
+        sudo vim "$CLASH_CONFIG_MIXIN" && {
+            _merge_config_restart && _okcat "配置更新成功，已重启生效"
+        }
+        ;;
+    -r)
+        less -f "$CLASH_CONFIG_RUNTIME"
+        ;;
+    *)
+        less -f "$CLASH_CONFIG_MIXIN"
+        ;;
+    esac
+}
+
+function clash-cli() {
+    case "$1" in
+    on)
+        _clash_on
+        ;;
+    off)
+        _clash_off
+        ;;
+    ui)
+        _clash_ui
+        ;;
+    status)
+        shift
+        _clash_status "$@"
+        ;;
+    proxy)
+        shift
+        _clash_proxy "$@"
+        ;;
+    tun)
+        shift
+        _clash_tun "$@"
+        ;;
+    mixin)
+        shift
+        _clash_mixin "$@"
+        ;;
+    secret)
+        shift
+        _clash_secret "$@"
+        ;;
+    update)
+        shift
+        _clash_update "$@"
+        ;;
+    *)
+        cat <<EOF
+
+Usage:
+    clash-cli COMMAND [OPTION]
+
+Commands:
+    on                      开启代理
+    off                     关闭代理
+    proxy    [on|off]       系统代理
+    ui                      面板地址
+    status                  内核状况
+    tun      [on|off]       Tun 模式
+    mixin    [-e|-r]        Mixin 配置
+    secret   [SECRET]       Web 密钥
+    update   [auto|log]     更新订阅
+
+EOF
+        ;;
+    esac
+}
